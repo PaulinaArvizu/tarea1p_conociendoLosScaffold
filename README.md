67853deabc885747fe95c1658d7e77a0a0d30cf5
PaulinaArvizu <is707571@iteso.mx>
2020-08-26 13:24:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tarea1p_conociendoLosScaffold
+ Tarea 1.p - Conociendo los Scaffold - Programación de Dispositivos Móviles
---
+++ android/app/src/main/kotlin/com/example/tarea1p_conociendoLosScaffold/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarea1p_conociendoLosScaffold
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
