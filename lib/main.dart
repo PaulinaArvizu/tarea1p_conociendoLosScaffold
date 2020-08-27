7bc8904971db64e25386a877d44b440dc0eee036
PaulinaArvizu <is707571@iteso.mx>
2020-08-27 00:26:50 -0500

    Mostrar snackbars y dialogo

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _colored = false;
   int _clicks = 0;
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Click the FAB'),
         actions: [
@@ -37,6 +39,51 @@ class _HomePageState extends State<HomePage> {
               setState(() {
                 _colored = !_colored;
                 _clicks++;
+                if (_clicks % 2 == 0) {
+                  //snackbar sin accion
+                  _scaffoldKey.currentState
+                    ..hideCurrentSnackBar() //ocultar snackbar anterior
+                    ..showSnackBar(
+                      //muestra snackbar
+                      SnackBar(
+                        content: Text("Número par. Snackbar sin accion."),
+                        duration: Duration(milliseconds: 2000),
+                      ),
+                    );
+                } else {
+                  //snackbar con accion
+                  _scaffoldKey.currentState
+                    ..hideCurrentSnackBar() //ocultar snackbar anterior
+                    ..showSnackBar(
+                      //muestra snackbar
+                      SnackBar(
+                        content: Text("Número impar. Snackbar con accion."),
+                        duration: Duration(milliseconds: 2000),
+                        action: SnackBarAction(
+                          label: 'Mostrar fecha',
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (_) {
+                                return AlertDialog(
+                                  title: Text("Fecha"),
+                                  content: Text(new DateTime.now().toString()),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text("Aceptar"),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                }
               });
             },
             color: _colored ? Colors.yellow : Colors.white,
@@ -45,7 +92,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
               'Numero de clicks: $_clicks',
